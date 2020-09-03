aad9461328eec1372792105b98488f05d07057c0
umairny <umairny1@gmail.com>
2020-09-02 23:47:48 -0400

    final

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("add_page", views.add_page, name="add_page"),
+    path("random", views.random, name="random"),
+    path("edit/<str:title>", views.edit, name="edit"),
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
+from random import randint
+import re
+
+
+from markdown2 import Markdown
+
+markdowner = Markdown()
+entries = util.list_entries()
+
+class Post(forms.Form):
+    title = forms.CharField(label= "Title")
+    textarea = forms.CharField(widget=forms.Textarea(), label='')
+
+class Edit(forms.Form):
+    textarea = forms.CharField(widget=forms.Textarea(), label='')
 
 
 def index(request):
@@ -8,3 +23,104 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+# Search logic
+def search(request):
+    query = request.GET.get('q').lower()
+    entry = [item.lower() for item in entries]
+    searchlist = []
+
+    if query:
+        if query in entry:
+            page = util.get_entry(query)
+            page_converted = markdowner.convert(page)
+            context = {
+                'page': page_converted,
+                'title': query,
+                }
+            return render(request, "encyclopedia/entry.html", context)
+            if query in entry:
+                searchlist.append(entry)
+                context = {
+                    'searchlist': searchlist
+                }
+                return render(request, "encyclopedia/search.html", context)
+        else:
+            return render(request, "encyclopedia/error.html", {"message": "The search result not found Try again"})
+    else:
+        return render(request, "encyclopedia/error.html", {"message": "Type some thing in search"})
+
+# Entry page 
+def entry(request, title):
+    if title in entries:
+        page = util.get_entry(title)
+        page_converted = markdowner.convert(page)
+        context = {
+            'page': page_converted,
+            'title': title,
+        }
+        return render(request, "encyclopedia/entry.html", context)
+    else:
+        return render(request, "encyclopedia/error.html", {"message": "The requested page was not found."})
+
+#Creat New page entry
+def add_page(request):
+    if request.method == 'POST':
+        form = Post(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            textarea = form.cleaned_data["textarea"]
+            if title in entries:
+                return render(request, "encyclopedia/error.html", {"message": "Page already exist"})
+            else:
+                util.save_entry(title, textarea)
+                page = util.get_entry(title)
+                page_converted = markdowner.convert(page)
+
+                context = {
+                    'page': page_converted,
+                    'title': title
+                }
+
+                return render(request, "encyclopedia/entry.html", context)
+    else:
+        return render(request, "encyclopedia/add_page.html", {"post": Post()})
+
+def edit(request, title):
+    if request.method == 'GET':
+        page = util.get_entry(title)
+        
+        context = {
+            
+            'edit': Edit(initial={'textarea': page}),
+            'title': title
+        }
+
+        return render(request, "encyclopedia/edit.html", context)
+    else:
+        form = Edit(request.POST) 
+        if form.is_valid():
+            textarea = form.cleaned_data["textarea"]
+            util.save_entry(title,textarea)
+            page = util.get_entry(title)
+            page_converted = markdowner.convert(page)
+
+            context = {
+                'page': page_converted,
+                'title': title
+            }
+
+            return render(request, "encyclopedia/entry.html", context)
+
+def random(request):
+
+    num = randint(0, len(entries) - 1)
+    page_random = entries[num]
+    
+    page = util.get_entry(page_random)
+    page_converted = markdowner.convert(page)
+    context = {
+        'page': page_converted,
+        'title': page_random,
+        }
+    return render(request, "encyclopedia/entry.html", context)
